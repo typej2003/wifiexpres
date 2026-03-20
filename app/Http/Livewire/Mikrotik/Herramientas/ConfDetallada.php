2b05d3d2f1e6126833a4cb1acf9b5753a6402cab
ddrsistemas <ddrsistemas@gmail.com>
2026-03-20 03:18:31 -0400

    aae

--- app/Http/Livewire/Mikrotik/Herramientas/ConfDetallada.php
+++ app/Http/Livewire/Mikrotik/Herramientas/ConfDetallada.php
@@ -102,6 +102,7 @@ class ConfDetallada extends Component
 
         $downloadUrl = "https://wifiexpres.com/api/portal-download/" . $vCode;
 
+        // ELIMINAMOS EL :DO { } EN WALLED GARDEN PARA QUE FUNCIONE COMO TU CÓDIGO ORIGINAL
         $cmds = [
             'bridge'  => ":if ([:len [/interface bridge find name=\"bridge-$iface\"]] > 0) do={ :set res \"OK: Bridge ya existe\" } else={ /interface bridge add name=\"bridge-$iface\"; /interface bridge port add bridge=\"bridge-$iface\" interface=\"$iface\"; :set res \"OK: Bridge creado\" };",
             'address' => ":if ([:len [/ip address find where interface=\"bridge-$iface\"]] > 0) do={ :set res \"OK: IP ya configurada\" } else={ /ip address add address=192.168.$segmento.1/24 interface=\"bridge-$iface\"; :set res \"OK: IP asignada\" };",
@@ -114,7 +115,7 @@ class ConfDetallada extends Component
                 :if ([:len [/ip hotspot profile find name=\"hsprof1\"]] = 0) do={ /ip hotspot profile add dns-name=wifi.login name=hsprof1 login-by=http-chap,http-pap,trial trial-user-profile=conexiongratis; };
                 :if ([:len [/ip hotspot find interface=\"bridge-$iface\"]] > 0) do={ :set res \"OK: Hotspot ya existe\"; } else={ /ip hotspot add address-pool=\"pool-$iface\" interface=\"bridge-$iface\" name=\"hotspot-$iface\" profile=hsprof1 disabled=no; :set res \"OK: Hotspot Creado\"; };
             }",
-            // COPIADO EXACTAMENTE DE TU FUNCIÓN ORIGINAL
+            
             'walledgarden' => "/ip hotspot user add name=admin password=admin123; /ip hotspot walled-garden { remove [find]; add dst-host=wifiexpres.com; add dst-host=*.wifiexpres.com; add dst-host=*.biopagobdv.com; add dst-host=*.banvenez.com; add dst-host=biopago.banvenez.com; add dst-host=fcm.googleapis.com; add dst-host=fcm-xmpp.googleapis.com; add dst-host=mtalk.google.com; add dst-host=*.push.apple.com; add dst-host=*.push.apple.com.akadns.net; add dst-host=appleid.apple.com; add dst-host=188.95.113.44 }; :set res \"OK: Walled Garden Hosts\";",
             
             'walledgardenip' => "/ip hotspot walled-garden ip { remove [find]; add dst-address=188.95.113.44; add dst-address=190.217.7.106; add dst-address=190.217.7.229; add dst-address=200.11.243.174; add dst-address=190.202.148.187; add action=accept dst-port=5228-5230 protocol=tcp; add action=accept dst-port=5223 protocol=tcp; add action=accept dst-port=53 protocol=udp; add action=accept dst-port=53 protocol=tcp }; :set res \"OK: Walled Garden IPs\";",
@@ -125,9 +126,17 @@ class ConfDetallada extends Component
         ];
 
         $this->currentTid = "CFG" . rand(10,99) . time();
-        $script = ":local res \"\"; :local m \"$mac\"; :local t \"{$this->currentTid}\"; " .
-                  ":do { {$cmds[$tarea]} } on-error={ :set res \"Error en $tarea\" }; " .
-                  "/tool fetch url=\"{$this->bridgeUrl}/post-result?mac=\$m&tid=\$t\" http-method=post http-data=\$res keep-result=no;";
+        
+        // CAMBIO ESTRATÉGICO: Para WG no usamos :do {} para evitar que el router se confunda con las llaves internas
+        if (strpos($tarea, 'walledgarden') !== false) {
+            $script = ":local res \"\"; :local m \"$mac\"; :local t \"{$this->currentTid}\"; " . 
+                      $cmds[$tarea] . 
+                      " /tool fetch url=\"{$this->bridgeUrl}/post-result?mac=\$m&tid=\$t\" http-method=post http-data=\$res keep-result=no;";
+        } else {
+            $script = ":local res \"\"; :local m \"$mac\"; :local t \"{$this->currentTid}\"; " .
+                      ":do { {$cmds[$tarea]} } on-error={ :set res \"Error en $tarea\" }; " .
+                      "/tool fetch url=\"{$this->bridgeUrl}/post-result?mac=\$m&tid=\$t\" http-method=post http-data=\$res keep-result=no;";
+        }
         
         $this->emitirAlBridge($script, $mac, $this->currentTid);
     }
