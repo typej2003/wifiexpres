828ced21a12b2dcab5fe96f90262df2a3e7fc0ea
ddrsistemas <ddrsistemas@gmail.com>
2026-03-20 01:48:19 -0400

    AAE

--- app/Http/Livewire/Mikrotik/Herramientas/ConfDetallada.php
+++ app/Http/Livewire/Mikrotik/Herramientas/ConfDetallada.php
@@ -105,16 +105,15 @@ class ConfDetallada extends Component
             'address' => ":if ([:len [/ip address find where interface=\"bridge-$iface\"]] > 0) do={ :set res \"OK: IP ya configurada\" } else={ /ip address add address=192.168.$segmento.1/24 interface=\"bridge-$iface\"; :set res \"OK: IP asignada\" };",
             'pool'    => ":if ([:len [/ip pool find name=\"pool-$iface\"]] > 0) do={ :set res \"OK: Pool ya existe\" } else={ /ip pool add name=\"pool-$iface\" ranges=192.168.$segmento.10-192.168.$segmento.250; :set res \"OK: Pool creado\" };",
             'dhcp'    => ":if ([:len [/ip dhcp-server find interface=\"bridge-$iface\"]] > 0) do={ :set res \"OK: DHCP ya existe\" } else={ /ip dhcp-server add address-pool=\"pool-$iface\" interface=\"bridge-$iface\" name=\"srv-$iface\" disabled=no; /ip dhcp-server network add address=192.168.$segmento.0/24 gateway=192.168.$segmento.1 dns-server=8.8.8.8; :set res \"OK: DHCP activo\" };",
-            'hotspot' => "{
-                :if ([:len [/ip hotspot user profile find name=\"neutro\"]] = 0) do={ /ip hotspot user profile add name=\"neutro\" shared-users=1 session-timeout=1s idle-timeout=1s; } else={ /ip hotspot user profile set [find name=\"neutro\"] session-timeout=1s idle-timeout=1s; };
-                :if ([:len [/ip hotspot user profile find name=\"cortesia 20min-0\"]] = 0) do={ /ip hotspot user profile add name=\"cortesia 20min-0\" shared-users=1 status-autorefresh=1m keepalive-timeout=2m; };
-                :if ([:len [/ip hotspot user profile find name=\"conexiongratis\"]] = 0) do={ /ip hotspot user profile add name=\"conexiongratis\" shared-users=1 rate-limit=\"2M/2M\"; };
-                :if ([:len [/ip hotspot profile find name=\"hsprof1\"]] = 0) do={ 
-                    /ip hotspot profile add dns-name=wifi.login name=hsprof1 login-by=http-chap,http-pap,trial,cookie,mac-cookie trial-user-profile=conexiongratis;
-                };
-                :if ([:len [/ip hotspot find interface=\"bridge-$iface\"]] > 0) do={ :set res \"OK: Hotspot ya existe\"; } else={ 
-                    /ip hotspot add address-pool=\"pool-$iface\" interface=\"bridge-$iface\" name=\"hotspot-$iface\" profile=hsprof1 disabled=no;
-                    :set res \"OK: Configuración Completa\";
+            'dhcp' => "{
+                :if ([:len [/ip dhcp-server find interface=\"bridge-$iface\"]] > 0) do={ 
+                    :set res \"OK: DHCP ya existe\" 
+                } else={ 
+                    :do {
+                        /ip dhcp-server add address-pool=\"pool-$iface\" interface=\"bridge-$iface\" name=\"srv-$iface\" disabled=no; 
+                        /ip dhcp-server network add address=192.168.$segmento.0/24 gateway=192.168.$segmento.1 dns-server=8.8.8.8; 
+                        :set res \"OK: DHCP activo\" 
+                    } on-error={ :set res \"Error: Interfaz bridge-$iface no lista\" }
                 };
             }"
         ];
