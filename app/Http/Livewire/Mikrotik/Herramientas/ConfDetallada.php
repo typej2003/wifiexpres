808ee867f2be9d39f671d4056f2065b252ac8953
ddrsistemas <ddrsistemas@gmail.com>
2026-03-20 01:13:51 -0400

    aw

--- app/Http/Livewire/Mikrotik/Herramientas/ConfDetallada.php
+++ app/Http/Livewire/Mikrotik/Herramientas/ConfDetallada.php
@@ -63,7 +63,7 @@ class ConfDetallada extends Component
     public function ejecutarTarea($iface, $index, $tarea)
     {
         $this->taskStatus[$iface][$tarea] = 'loading';
-        $this->taskResult[$iface][$tarea] = 'Procesando dependencias...';
+        $this->taskResult[$iface][$tarea] = 'Configurando perfiles y Hotspot...';
         $this->activeTask = ['iface' => $iface, 'tarea' => $tarea];
         
         $router = Router::findOrFail($this->router_id);
@@ -77,17 +77,15 @@ class ConfDetallada extends Component
             'pool'    => ":if ([:len [/ip pool find name=\"pool-$iface\"]] > 0) do={ :set res \"OK: Pool ya existe\" } else={ /ip pool add name=\"pool-$iface\" ranges=192.168.$segmento.10-192.168.$segmento.250; :set res \"OK: Pool creado\" };",
             'dhcp'    => ":if ([:len [/ip dhcp-server find interface=\"bridge-$iface\"]] > 0) do={ :set res \"OK: DHCP ya existe\" } else={ /ip dhcp-server add address-pool=\"pool-$iface\" interface=\"bridge-$iface\" name=\"srv-$iface\" disabled=no; /ip dhcp-server network add address=192.168.$segmento.0/24 gateway=192.168.$segmento.1 dns-server=8.8.8.8; :set res \"OK: DHCP activo\" };",
             'hotspot' => "{
-                :if ([:len [/ip hotspot user profile find name=\"neutro\"]] = 0) do={ /ip hotspot user profile add name=\"neutro\" shared-users=1 };
-                :if ([:len [/ip hotspot user profile find name=\"cortesia 20min-0\"]] = 0) do={ /ip hotspot user profile add name=\"cortesia 20min-0\" session-timeout=20m shared-users=1 };
-                :if ([:len [/ip hotspot user profile find name=\"conexiongratis\"]] = 0) do={ /ip hotspot user profile add name=\"conexiongratis\" shared-users=1 rate-limit=\"2M/2M\" };
+                :if ([:len [/ip hotspot user profile find name=\"neutro\"]] = 0) do={ /ip hotspot user profile add name=\"neutro\" shared-users=1 session-timeout=1s idle-timeout=1s; } else={ /ip hotspot user profile set [find name=\"neutro\"] session-timeout=1s idle-timeout=1s; };
+                :if ([:len [/ip hotspot user profile find name=\"cortesia 20min-0\"]] = 0) do={ /ip hotspot user profile add name=\"cortesia 20min-0\" shared-users=1 status-autorefresh=1m keepalive-timeout=2m; };
+                :if ([:len [/ip hotspot user profile find name=\"conexiongratis\"]] = 0) do={ /ip hotspot user profile add name=\"conexiongratis\" shared-users=1 rate-limit=\"2M/2M\"; };
                 :if ([:len [/ip hotspot profile find name=\"hsprof1\"]] = 0) do={ 
                     /ip hotspot profile add dns-name=wifi.login name=hsprof1 login-by=http-chap,http-pap,trial,cookie,mac-cookie trial-user-profile=conexiongratis;
                 };
-                :if ([:len [/ip hotspot find interface=\"bridge-$iface\"]] > 0) do={ 
-                    :set res \"OK: Hotspot ya existe\";
-                } else={ 
+                :if ([:len [/ip hotspot find interface=\"bridge-$iface\"]] > 0) do={ :set res \"OK: Hotspot ya existe\"; } else={ 
                     /ip hotspot add address-pool=\"pool-$iface\" interface=\"bridge-$iface\" name=\"hotspot-$iface\" profile=hsprof1 disabled=no;
-                    :set res \"OK: Hotspot listo con Cookies\";
+                    :set res \"OK: Hotspot con Cookies y Tiempos OK\";
                 };
             }"
         ];
@@ -95,7 +93,7 @@ class ConfDetallada extends Component
         $this->currentTid = "CFG" . time();
         $this->intentos = 0;
         $script = ":local res \"\"; :local m \"$mac\"; :local t \"{$this->currentTid}\"; " .
-                  ":do { {$cmds[$tarea]} } on-error={ :set res \"Error: Falta IP o Pool\" }; " .
+                  ":do { {$cmds[$tarea]} } on-error={ :set res \"Error en ejecucion\" }; " .
                   "/tool fetch url=\"{$this->bridgeUrl}/post-result?mac=\$m&tid=\$t\" http-method=post http-data=\$res keep-result=no;";
         $this->emitirAlBridge($script, $mac, $this->currentTid);
     }
