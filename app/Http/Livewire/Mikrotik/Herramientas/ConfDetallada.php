d9735b81de8d2cc4e7a6b659a894d92252148993
ddrsistemas <ddrsistemas@gmail.com>
2026-03-21 14:21:57 -0400

    red

--- app/Http/Livewire/Mikrotik/Herramientas/ConfDetallada.php
+++ app/Http/Livewire/Mikrotik/Herramientas/ConfDetallada.php
@@ -130,8 +130,6 @@ class ConfDetallada extends Component
             'pool'    => "
                 :if ([:len [/ip pool find name=\"pool-$iface\"]] = 0) do={ /ip pool add name=\"pool-$iface\" ranges=192.168.$segmento.10-192.168.$segmento.250 };
             ",
-            
-            // DHCP AJUSTADO PARA ROUTER LIMPIO (hAP lite)
             'dhcp'    => "
                 :delay 2s;
                 /ip dhcp-server network add address=192.168.$segmento.0/24 gateway=192.168.$segmento.1 dns-server=8.8.8.8;
@@ -139,22 +137,26 @@ class ConfDetallada extends Component
                 /ip dhcp-server add address-pool=\"pool-$iface\" interface=\"bridge-$iface\" name=\"srv-$iface\" disabled=no;
             ",
             
+            // HOTSPOT REFORZADO CON DELAYS Y VERIFICACIÓN
             'hotspot' => "
+                :delay 2s;
                 :do { /ip hotspot user profile remove [find where name~\"neutro|cortesia|conexion\"] } on-error={};
                 /ip hotspot user profile add name=\"neutro\" shared-users=1 session-timeout=1s;
                 /ip hotspot user profile add name=\"cortesia 20min-0\" shared-users=1 session-timeout=20m;
                 /ip hotspot user profile add name=\"conexiongratis\" shared-users=1 rate-limit=\"2M/2M\";
+                :delay 1s;
                 :if ([:len [/ip hotspot profile find name=\"hsprof-$iface\"]] = 0) do={
                     /ip hotspot profile add dns-name=wifi.login name=\"hsprof-$iface\" login-by=http-chap,http-pap,trial trial-user-profile=conexiongratis;
                 };
+                :delay 1s;
                 :if ([:len [/ip hotspot find interface=\"bridge-$iface\"]] = 0) do={
                     /ip hotspot add address-pool=\"pool-$iface\" interface=\"bridge-$iface\" name=\"hotspot-$iface\" profile=\"hsprof-$iface\" disabled=no;
                 };
             ",
+            
             'wg_servidor' => "/ip hotspot walled-garden remove [find dst-host=\"wifiexpres.com\" or dst-host=\"*.wifiexpres.com\" or dst-host=\"188.95.113.44\"]; /ip hotspot walled-garden add dst-host=wifiexpres.com; /ip hotspot walled-garden add dst-host=*.wifiexpres.com; /ip hotspot walled-garden add dst-host=188.95.113.44; :do { /ip hotspot walled-garden ip remove [find dst-address=188.95.113.44] } on-error={}; /ip hotspot walled-garden ip add dst-address=188.95.113.44 dst-port=3000 protocol=tcp comment=\"Acceso Bridge Nodejs\"",
             'wg_bdv' => "/ip hotspot walled-garden remove [find comment=\"Pasarela BDV\"]; /ip hotspot walled-garden add dst-host=*.biopagobdv.com action=allow comment=\"Pasarela BDV\"; /ip hotspot walled-garden add dst-host=*.banvenez.com action=allow comment=\"Pasarela BDV\"; /ip hotspot walled-garden add dst-host=biopago.banvenez.com action=allow comment=\"Pasarela BDV\"; /ip hotspot walled-garden ip remove [find comment=\"Pasarela BDV\"]; /ip hotspot walled-garden ip add dst-address=190.217.7.106 action=accept comment=\"Pasarela BDV\"; /ip hotspot walled-garden ip add dst-address=190.217.7.229 action=accept comment=\"Pasarela BDV\"; /ip hotspot walled-garden ip add dst-address=200.11.243.174 action=accept comment=\"Pasarela BDV\"; /ip hotspot walled-garden ip add dst-address=190.202.148.187 action=accept comment=\"Pasarela BDV\"",
             'wg_push' => "/ip hotspot walled-garden remove [find comment=\"Notificaciones Push\"]; /ip hotspot walled-garden add dst-host=fcm.googleapis.com action=allow comment=\"Notificaciones Push\"; /ip hotspot walled-garden add dst-host=fcm-xmpp.googleapis.com action=allow comment=\"Notificaciones Push\"; /ip hotspot walled-garden add dst-host=mtalk.google.com action=allow comment=\"Notificaciones Push\"; /ip hotspot walled-garden add dst-host=*.push.apple.com action=allow comment=\"Notificaciones Push\"; /ip hotspot walled-garden add dst-host=*.push.apple.com.akadns.net action=allow comment=\"Notificaciones Push\"; /ip hotspot walled-garden add dst-host=appleid.apple.com action=allow comment=\"Notificaciones Push\"; /ip hotspot walled-garden ip remove [find comment=\"Notificaciones Push\"]; /ip hotspot walled-garden ip add dst-port=5228-5230 protocol=tcp action=accept comment=\"Notificaciones Push\"; /ip hotspot walled-garden ip add dst-port=5223 protocol=tcp action=accept comment=\"Notificaciones Push\"",
-            
             'portal' => "
                 :do { /resolve wifiexpres.com } on-error={};
                 :do { /file remove [find name=\"hotspot/login.html\"] } on-error={};
@@ -166,7 +168,6 @@ class ConfDetallada extends Component
                     /file set [find name=\"login_temp.html\"] name=\"hotspot/login.html\";
                 }
             ",
-            
             'reboot' => "/system reboot"
         ];
 
