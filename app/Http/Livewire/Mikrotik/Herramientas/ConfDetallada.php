4130c8be8e6079b0ce624e108518cf64db93cc7a
ddrsistemas <ddrsistemas@gmail.com>
2026-03-21 11:50:00 -0400

    ere

--- app/Http/Livewire/Mikrotik/Herramientas/ConfDetallada.php
+++ app/Http/Livewire/Mikrotik/Herramientas/ConfDetallada.php
@@ -59,6 +59,7 @@ class ConfDetallada extends Component
         $this->isProcessing = true;
         $this->queue = [];
         
+        // SECUENCIA LÓGICA
         $tareas = ['limpiar_interfaz', 'bridge', 'address', 'pool', 'dhcp', 'hotspot'];
         foreach ($tareas as $t) {
             $this->queue[] = ['iface' => $iface, 'index' => $index, 'tarea' => $t];
@@ -84,6 +85,7 @@ class ConfDetallada extends Component
             $next = array_shift($this->queue);
             $this->ejecutarTarea($next['iface'], $next['index'], $next['tarea']);
         } else {
+            // FIN DE PROCESO: Apagamos flags para detener el poll
             $this->isProcessing = false;
             $this->isWaitingResponse = false;
             $this->activeTask = null;
@@ -133,25 +135,11 @@ class ConfDetallada extends Component
                 /ip hotspot add address-pool=\"pool-$iface\" interface=\"bridge-$iface\" name=\"hotspot-$iface\" profile=\"hsprof-$iface\" disabled=no
             ",
             'wg_servidor' => "/ip hotspot walled-garden remove [find dst-host=\"wifiexpres.com\" or dst-host=\"*.wifiexpres.com\" or dst-host=\"188.95.113.44\"]; /ip hotspot walled-garden add dst-host=wifiexpres.com; /ip hotspot walled-garden add dst-host=*.wifiexpres.com; /ip hotspot walled-garden add dst-host=188.95.113.44; :do { /ip hotspot walled-garden ip remove [find dst-address=188.95.113.44] } on-error={}; /ip hotspot walled-garden ip add dst-address=188.95.113.44 dst-port=3000 protocol=tcp comment=\"Acceso Bridge Nodejs\"",
-            
-            // COMANDO RESTAURADO SEGÚN TU VERSIÓN ORIGINAL
             'wg_bdv' => "/ip hotspot walled-garden remove [find comment=\"Pasarela BDV\"]; /ip hotspot walled-garden add dst-host=*.biopagobdv.com action=allow comment=\"Pasarela BDV\"; /ip hotspot walled-garden add dst-host=*.banvenez.com action=allow comment=\"Pasarela BDV\"; /ip hotspot walled-garden add dst-host=biopago.banvenez.com action=allow comment=\"Pasarela BDV\"; /ip hotspot walled-garden ip remove [find comment=\"Pasarela BDV\"]; /ip hotspot walled-garden ip add dst-address=190.217.7.106 action=accept comment=\"Pasarela BDV\"; /ip hotspot walled-garden ip add dst-address=190.217.7.229 action=accept comment=\"Pasarela BDV\"; /ip hotspot walled-garden ip add dst-address=200.11.243.174 action=accept comment=\"Pasarela BDV\"; /ip hotspot walled-garden ip add dst-address=190.202.148.187 action=accept comment=\"Pasarela BDV\"",
-            
-            // COMANDO RESTAURADO SEGÚN TU VERSIÓN ORIGINAL
             'wg_push' => "/ip hotspot walled-garden remove [find comment=\"Notificaciones Push\"]; /ip hotspot walled-garden add dst-host=fcm.googleapis.com action=allow comment=\"Notificaciones Push\"; /ip hotspot walled-garden add dst-host=fcm-xmpp.googleapis.com action=allow comment=\"Notificaciones Push\"; /ip hotspot walled-garden add dst-host=mtalk.google.com action=allow comment=\"Notificaciones Push\"; /ip hotspot walled-garden add dst-host=*.push.apple.com action=allow comment=\"Notificaciones Push\"; /ip hotspot walled-garden add dst-host=*.push.apple.com.akadns.net action=allow comment=\"Notificaciones Push\"; /ip hotspot walled-garden add dst-host=appleid.apple.com action=allow comment=\"Notificaciones Push\"; /ip hotspot walled-garden ip remove [find comment=\"Notificaciones Push\"]; /ip hotspot walled-garden ip add dst-port=5228-5230 protocol=tcp action=accept comment=\"Notificaciones Push\"; /ip hotspot walled-garden ip add dst-port=5223 protocol=tcp action=accept comment=\"Notificaciones Push\"",
             
-            // PORTAL REFORZADO (Funcional)
-            'portal' => "
-                :do { /resolve wifiexpres.com } on-error={};
-                :do { /file remove [find name=\"hotspot/login.html\"] } on-error={};
-                :do { /file remove [find name=\"login_temp.html\"] } on-error={};
-                /ip hotspot profile set [find name=\"hsprof1\" or name=\"hsprof-$iface\"] html-directory=hotspot;
-                /tool fetch url=\"$downloadUrl\" dst-path=\"login_temp.html\" check-certificate=no;
-                :delay 3s;
-                :if ([:len [/file find name=\"login_temp.html\"]] > 0) do={
-                    /file set [find name=\"login_temp.html\"] name=\"hotspot/login.html\";
-                }
-            ",
+            // PORTAL ACTUALIZADO: Descarga a archivo temporal y lo mueve a la carpeta final
+            'portal' => "/ip hotspot profile set [find name=\"hsprof1\" or name=\"hsprof-$iface\"] html-directory=hotspot; /tool fetch url=\"$downloadUrl\" dst-path=\"login_temp.html\" check-certificate=no; :delay 2s; :do { /file remove [find name=\"hotspot/login.html\"] } on-error={}; /file set [find name=\"login_temp.html\"] name=\"hotspot/login.html\"",
             
             'reboot' => "/system reboot"
         ];
@@ -206,6 +194,7 @@ class ConfDetallada extends Component
             $this->taskStatus[$iface][$tarea] = $status;
             $this->taskResult[$iface][$tarea] = $mensaje;
             
+            // Apagamos la espera momentáneamente
             $this->isWaitingResponse = false;
             $this->activeTask = null;
             $this->intentos = 0;
