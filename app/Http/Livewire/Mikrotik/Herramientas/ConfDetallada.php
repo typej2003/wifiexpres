e527c67b711c2caf30ee33fd30add7fb0f2de673
ddrsistemas <ddrsistemas@gmail.com>
2026-03-21 11:48:08 -0400

    wer

--- app/Http/Livewire/Mikrotik/Herramientas/ConfDetallada.php
+++ app/Http/Livewire/Mikrotik/Herramientas/ConfDetallada.php
@@ -102,9 +102,12 @@ class ConfDetallada extends Component
         $mac = strtoupper(trim($router->macAddress));
         $segmento = ($index + 1) * 10;
         
-        // Obtenemos el ID de la versión para la URL
-        $vUrlId = $this->version_id ?: 1;
-        $downloadUrl = "https://wifiexpres.com/api/portal-download/" . $vUrlId;
+        $vCode = 1;
+        if($this->version_id) {
+            $versionObj = HotspotVersion::find($this->version_id);
+            $vCode = $versionObj ? $versionObj->code : 1;
+        }
+        $downloadUrl = "https://wifiexpres.com/api/portal-download/" . $vCode;
 
         $cmds = [
             'limpiar_interfaz' => "
@@ -130,15 +133,24 @@ class ConfDetallada extends Component
                 /ip hotspot add address-pool=\"pool-$iface\" interface=\"bridge-$iface\" name=\"hotspot-$iface\" profile=\"hsprof-$iface\" disabled=no
             ",
             'wg_servidor' => "/ip hotspot walled-garden remove [find dst-host=\"wifiexpres.com\" or dst-host=\"*.wifiexpres.com\" or dst-host=\"188.95.113.44\"]; /ip hotspot walled-garden add dst-host=wifiexpres.com; /ip hotspot walled-garden add dst-host=*.wifiexpres.com; /ip hotspot walled-garden add dst-host=188.95.113.44; :do { /ip hotspot walled-garden ip remove [find dst-address=188.95.113.44] } on-error={}; /ip hotspot walled-garden ip add dst-address=188.95.113.44 dst-port=3000 protocol=tcp comment=\"Acceso Bridge Nodejs\"",
+            
+            // COMANDO RESTAURADO SEGÚN TU VERSIÓN ORIGINAL
             'wg_bdv' => "/ip hotspot walled-garden remove [find comment=\"Pasarela BDV\"]; /ip hotspot walled-garden add dst-host=*.biopagobdv.com action=allow comment=\"Pasarela BDV\"; /ip hotspot walled-garden add dst-host=*.banvenez.com action=allow comment=\"Pasarela BDV\"; /ip hotspot walled-garden add dst-host=biopago.banvenez.com action=allow comment=\"Pasarela BDV\"; /ip hotspot walled-garden ip remove [find comment=\"Pasarela BDV\"]; /ip hotspot walled-garden ip add dst-address=190.217.7.106 action=accept comment=\"Pasarela BDV\"; /ip hotspot walled-garden ip add dst-address=190.217.7.229 action=accept comment=\"Pasarela BDV\"; /ip hotspot walled-garden ip add dst-address=200.11.243.174 action=accept comment=\"Pasarela BDV\"; /ip hotspot walled-garden ip add dst-address=190.202.148.187 action=accept comment=\"Pasarela BDV\"",
+            
+            // COMANDO RESTAURADO SEGÚN TU VERSIÓN ORIGINAL
             'wg_push' => "/ip hotspot walled-garden remove [find comment=\"Notificaciones Push\"]; /ip hotspot walled-garden add dst-host=fcm.googleapis.com action=allow comment=\"Notificaciones Push\"; /ip hotspot walled-garden add dst-host=fcm-xmpp.googleapis.com action=allow comment=\"Notificaciones Push\"; /ip hotspot walled-garden add dst-host=mtalk.google.com action=allow comment=\"Notificaciones Push\"; /ip hotspot walled-garden add dst-host=*.push.apple.com action=allow comment=\"Notificaciones Push\"; /ip hotspot walled-garden add dst-host=*.push.apple.com.akadns.net action=allow comment=\"Notificaciones Push\"; /ip hotspot walled-garden add dst-host=appleid.apple.com action=allow comment=\"Notificaciones Push\"; /ip hotspot walled-garden ip remove [find comment=\"Notificaciones Push\"]; /ip hotspot walled-garden ip add dst-port=5228-5230 protocol=tcp action=accept comment=\"Notificaciones Push\"; /ip hotspot walled-garden ip add dst-port=5223 protocol=tcp action=accept comment=\"Notificaciones Push\"",
             
-            // PORTAL CORREGIDO: Asegura directorio, descarga y aplica perfil
+            // PORTAL REFORZADO (Funcional)
             'portal' => "
                 :do { /resolve wifiexpres.com } on-error={};
-                :if ([:len [/file find name=\"hotspot\"]] = 0) do={/file add name=hotspot type=directory};
-                /ip hotspot profile set [find name~\"hsprof\"] html-directory=hotspot;
-                /tool fetch url=\"$downloadUrl\" dst-path=\"hotspot/login.html\" check-certificate=no;
+                :do { /file remove [find name=\"hotspot/login.html\"] } on-error={};
+                :do { /file remove [find name=\"login_temp.html\"] } on-error={};
+                /ip hotspot profile set [find name=\"hsprof1\" or name=\"hsprof-$iface\"] html-directory=hotspot;
+                /tool fetch url=\"$downloadUrl\" dst-path=\"login_temp.html\" check-certificate=no;
+                :delay 3s;
+                :if ([:len [/file find name=\"login_temp.html\"]] > 0) do={
+                    /file set [find name=\"login_temp.html\"] name=\"hotspot/login.html\";
+                }
             ",
             
             'reboot' => "/system reboot"
