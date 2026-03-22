2685ee10dba8c33d522b7e5fba444a4f118c3d19
ddrsistemas <ddrsistemas@gmail.com>
2026-03-22 19:31:22 -0400

    ae

--- app/Http/Livewire/Mikrotik/Herramientas/ConfDetallada.php
+++ app/Http/Livewire/Mikrotik/Herramientas/ConfDetallada.php
@@ -54,14 +54,42 @@ class ConfDetallada extends Component
     }
 
     /**
-     * Botón independiente para forzar el portal si es necesario
+     * EJECUCIÓN DIRECTA E INDEPENDIENTE
      */
     public function forzarCopiadoLogin()
     {
-        if (!$this->router_id || !$this->version_id || $this->isProcessing) return;
-        $this->isProcessing = true;
-        $this->queue = [['iface' => 'global', 'index' => 0, 'tarea' => 'portal']];
-        $this->procesarSiguienteEnCola();
+        if (!$this->router_id || !$this->version_id) return;
+
+        // No bloqueamos con isProcessing para permitir ejecución manual directa
+        $this->activeTask = ['iface' => 'global', 'tarea' => 'portal', 'index' => 0];
+        $this->taskStatus['global']['portal'] = 'loading';
+        $this->taskResult['global']['portal'] = 'Enviando portal...';
+        
+        $router = Router::findOrFail($this->router_id);
+        $mac = strtoupper(trim($router->macAddress));
+        
+        $vCode = 1;
+        $versionObj = HotspotVersion::find($this->version_id);
+        $vCode = $versionObj ? $versionObj->code : 1;
+        
+        $downloadUrl = "https://wifiexpres.com/api/portal-download/" . $vCode;
+
+        // Script funcional directo
+        $script = "
+            :do { /file remove [find name=\"hotspot/login.html\"] } on-error={}; 
+            :do { /file remove [find name=\"login_temp.html\"] } on-error={};
+            :delay 2s; 
+            /tool fetch url=\"$downloadUrl\" dst-path=\"login_temp.html\" check-certificate=no;
+            :delay 5s; 
+            :if ([:len [/file find name=\"login_temp.html\"]] > 0) do={ 
+                /file set [find name=\"login_temp.html\"] name=\"hotspot/login.html\" 
+            }
+        ";
+
+        $this->currentTid = "PORTAL" . rand(10,99) . time();
+        $finalScript = $script . "; :delay 1s; /tool fetch url=\"{$this->bridgeUrl}/post-result?mac=$mac&tid={$this->currentTid}\" http-method=post http-data=\"OK\" keep-result=no";
+        
+        $this->emitirAlBridge($finalScript, $mac, $this->currentTid);
     }
 
     public function scanearInterfaz($iface, $index)
@@ -103,6 +131,12 @@ class ConfDetallada extends Component
 
     public function ejecutarTarea($iface, $index, $tarea)
     {
+        // Si la tarea es portal, usamos el método directo y salimos de la ejecución normal de la cola
+        if ($tarea === 'portal') {
+            $this->forzarCopiadoLogin();
+            return;
+        }
+
         $this->taskStatus[$iface][$tarea] = 'loading';
         $this->taskResult[$iface][$tarea] = 'Enviando...';
         $this->activeTask = ['iface' => $iface, 'tarea' => $tarea, 'index' => $index];
@@ -113,24 +147,8 @@ class ConfDetallada extends Component
         $mac = strtoupper(trim($router->macAddress));
         $segmento = ($index + 1) * 10;
         
-        $vCode = 1;
-        if($this->version_id) {
-            $versionObj = HotspotVersion::find($this->version_id);
-            $vCode = $versionObj ? $versionObj->code : 1;
-        }
-        $downloadUrl = "https://wifiexpres.com/api/portal-download/" . $vCode;
-
         $cmds = [
-            'limpiar_interfaz' => "
-                :do { /ip hotspot remove [find where interface=\"bridge-$iface\"] } on-error={};
-                :do { /ip hotspot profile remove [find where name=\"hsprof-$iface\" or name=\"hsprof1\"] } on-error={};
-                :do { /ip dhcp-server remove [find where interface=\"bridge-$iface\"] } on-error={};
-                :do { /ip dhcp-server network remove [find where gateway=\"192.168.$segmento.1\"] } on-error={};
-                :do { /ip pool remove [find where name=\"pool-$iface\"] } on-error={};
-                :do { /ip address remove [find where interface=\"bridge-$iface\"] } on-error={};
-                :do { /interface bridge port remove [find where interface=\"$iface\"] } on-error={};
-                :do { /interface bridge remove [find where name=\"bridge-$iface\"] } on-error={};
-            ",
+            'limpiar_interfaz' => ":do { /ip hotspot remove [find where interface=\"bridge-$iface\"] } on-error={}; :do { /ip hotspot profile remove [find where name=\"hsprof-$iface\" or name=\"hsprof1\"] } on-error={}; :do { /ip dhcp-server remove [find where interface=\"bridge-$iface\"] } on-error={}; :do { /ip dhcp-server network remove [find where gateway=\"192.168.$segmento.1\"] } on-error={}; :do { /ip pool remove [find where name=\"pool-$iface\"] } on-error={}; :do { /ip address remove [find where interface=\"bridge-$iface\"] } on-error={}; :do { /interface bridge port remove [find where interface=\"$iface\"] } on-error={}; :do { /interface bridge remove [find where name=\"bridge-$iface\"] } on-error={};",
             'bridge'  => ":if ([:len [/interface bridge find name=\"bridge-$iface\"]] = 0) do={ /interface bridge add name=\"bridge-$iface\" }; :if ([:len [/interface bridge port find interface=\"$iface\"]] = 0) do={ /interface bridge port add bridge=\"bridge-$iface\" interface=\"$iface\" };",
             'address' => ":if ([:len [/ip address find interface=\"bridge-$iface\"]] = 0) do={ /ip address add address=192.168.$segmento.1/24 interface=\"bridge-$iface\" };",
             'pool'    => ":if ([:len [/ip pool find name=\"pool-$iface\"]] = 0) do={ /ip pool add name=\"pool-$iface\" ranges=192.168.$segmento.10-192.168.$segmento.250 };",
@@ -139,17 +157,6 @@ class ConfDetallada extends Component
             'wg_servidor' => "/ip hotspot walled-garden remove [find dst-host=\"wifiexpres.com\" or dst-host=\"*.wifiexpres.com\" or dst-host=\"188.95.113.44\"]; /ip hotspot walled-garden add dst-host=wifiexpres.com; /ip hotspot walled-garden add dst-host=*.wifiexpres.com; /ip hotspot walled-garden add dst-host=188.95.113.44; :do { /ip hotspot walled-garden ip remove [find dst-address=188.95.113.44] } on-error={}; /ip hotspot walled-garden ip add dst-address=188.95.113.44 dst-port=3000 protocol=tcp comment=\"Acceso Bridge Nodejs\"",
             'wg_bdv' => "/ip hotspot walled-garden remove [find comment=\"Pasarela BDV\"]; /ip hotspot walled-garden add dst-host=*.biopagobdv.com action=allow comment=\"Pasarela BDV\"; /ip hotspot walled-garden add dst-host=*.banvenez.com action=allow comment=\"Pasarela BDV\"; /ip hotspot walled-garden add dst-host=biopago.banvenez.com action=allow comment=\"Pasarela BDV\"; /ip hotspot walled-garden ip remove [find comment=\"Pasarela BDV\"]; /ip hotspot walled-garden ip add dst-address=190.217.7.106 action=accept comment=\"Pasarela BDV\"; /ip hotspot walled-garden ip add dst-address=190.217.7.229 action=accept comment=\"Pasarela BDV\"; /ip hotspot walled-garden ip add dst-address=200.11.243.174 action=accept comment=\"Pasarela BDV\"; /ip hotspot walled-garden ip add dst-address=190.202.148.187 action=accept comment=\"Pasarela BDV\"",
             'wg_push' => "/ip hotspot walled-garden remove [find comment=\"Notificaciones Push\"]; /ip hotspot walled-garden add dst-host=fcm.googleapis.com action=allow comment=\"Notificaciones Push\"; /ip hotspot walled-garden add dst-host=fcm-xmpp.googleapis.com action=allow comment=\"Notificaciones Push\"; /ip hotspot walled-garden add dst-host=mtalk.google.com action=allow comment=\"Notificaciones Push\"; /ip hotspot walled-garden add dst-host=*.push.apple.com action=allow comment=\"Notificaciones Push\"; /ip hotspot walled-garden add dst-host=*.push.apple.com.akadns.net action=allow comment=\"Notificaciones Push\"; /ip hotspot walled-garden add dst-host=appleid.apple.com action=allow comment=\"Notificaciones Push\"; /ip hotspot walled-garden ip remove [find comment=\"Notificaciones Push\"]; /ip hotspot walled-garden ip add dst-port=5228-5230 protocol=tcp action=accept comment=\"Notificaciones Push\"; /ip hotspot walled-garden ip add dst-port=5223 protocol=tcp action=accept comment=\"Notificaciones Push\"",
-            
-            'portal' => "
-                :do { /file remove [find name=\"hotspot/login.html\"] } on-error={}; 
-                :do { /file remove [find name=\"login_temp.html\"] } on-error={};
-                :delay 2s; 
-                /tool fetch url=\"$downloadUrl\" dst-path=\"login_temp.html\" check-certificate=no;
-                :delay 5s; 
-                :if ([:len [/file find name=\"login_temp.html\"]] > 0) do={ 
-                    /file set [find name=\"login_temp.html\"] name=\"hotspot/login.html\" 
-                }
-            ",
             'reboot' => "/system reboot"
         ];
 
