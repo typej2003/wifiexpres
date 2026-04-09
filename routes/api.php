3ce6bd62a9f98479b315b96156096b852ecde664
ddrsistemas <ddrsistemas@gmail.com>
2026-04-09 14:02:08 -0400

    www

--- routes/api.php
+++ routes/api.php
@@ -111,6 +111,8 @@ Route::post('/auth-sync-service', function (Request $request) {
     $email = trim($request->input('email'));
     $password = $request->input('password');
 
+    return response()->json(['message' => $password], 401);
+
     $user = User::where('email', $email)->first();
 
     if (!$user) {
