0a87ce28df1b304d00997c1e26e73a042075d3fe
ddrsistemas <ddrsistemas@gmail.com>
2026-04-10 17:16:06 -0400

    aaa

--- app/Http/Controllers/Api/SyPagoController.php
+++ app/Http/Controllers/Api/SyPagoController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
+use App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
