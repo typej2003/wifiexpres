d6df371f019064111fb729f3bd00910c649f4b75
ddrsistemas <ddrsistemas@gmail.com>
2026-04-09 23:50:09 -0400

    aa

--- app/Http/Controllers/Api/V2/UserController.php
+++ app/Http/Controllers/Api/V2/UserController.php
@@ -166,8 +166,10 @@ class UserController extends Controller
             $tidFinal = "PRE" . time();
             
             // Comando optimizado estilo "Profile"
-            $cmdFinal = ":local m \"$mac\"; :local t \"$tidFinal\"; :local u \"$username\"; :local p \"$password\"; :local pr \"$profile\"; :do { /ip hotspot user add name=\$u password=\$p profile=\$pr; /tool fetch url=\"{$this->bridgeUrl}/post-result?mac=\$m&tid=\$t\" http-method=post http-data=\"OK\" keep-result=no; } on-error={ /tool fetch url=\"{$this->bridgeUrl}/post-result?mac=\$m&tid=\$t\" http-method=post http-data=\"FAIL\" keep-result=no; };";
+            //$cmdFinal = ":local m \"$mac\"; :local t \"$tidFinal\"; :local u \"$username\"; :local p \"$password\"; :local pr \"$profile\"; :do { /ip hotspot user add name=\$u password=\$p profile=\$pr; /tool fetch url=\"{$this->bridgeUrl}/post-result?mac=\$m&tid=\$t\" http-method=post http-data=\"OK\" keep-result=no; } on-error={ /tool fetch url=\"{$this->bridgeUrl}/post-result?mac=\$m&tid=\$t\" http-method=post http-data=\"FAIL\" keep-result=no; };";
 
+            $cmdFinal = ":local m \"$mac\"; :local t \"$tidFinal\"; :local u \"$username\"; :local p \"$password\"; :local pr \"$profile\"; :do { :local id [/ip hotspot user find name=\$u]; :if ([:len \$id] > 0) do={ /ip hotspot user set \$id password=\$p profile=\$pr; } else={ /ip hotspot user add name=\$u password=\$p profile=\$pr; }; /tool fetch url=\"{$this->bridgeUrl}/post-result?mac=\$m&tid=\$t\" http-method=post http-data=\"OK\" keep-result=no; } on-error={ /tool fetch url=\"{$this->bridgeUrl}/post-result?mac=\$m&tid=\$t\" http-method=post http-data=\"FAIL\" keep-result=no; };";
+            
             $this->emitirAlSocket($cmdFinal, $mac, $tidFinal);
             
             $confirmado = $this->esperarConfirmacion($mac, $tidFinal);
