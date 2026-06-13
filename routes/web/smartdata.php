e3c424ca3952575fb76e39d7fcd64e2778094db5
ddrsistemas <ddrsistemas@gmail.com>
2026-06-13 12:21:06 -0400

    subir

--- routes/web/smartdata.php
+++ routes/web/smartdata.php
@@ -7,7 +7,7 @@ use App\Http\Livewire\Mikrotik\Smartdata\PromocionesOfertas;
 use App\Http\Livewire\Mikrotik\Smartdata\Monitoreo;
 
 Route::middleware(['auth', 'role:aliadoSmartData,admin'])->group(function () {
-    Route::get('/smartdata/users-visits', UsersVisits::class)->name('smartdata.users-visits');
+    Route::get('/smartdata/users-visits/{userId?}', UsersVisits::class)->name('smartdata.users-visits');
     Route::get('/smartdata/permanencia', Permanencia::class)->name('smartdata.permanencia');
     Route::get('/smartdata/promociones', PromocionesOfertas::class)->name('smartdata.promociones');
     Route::get('/smartdata/monitoreo', Monitoreo::class)->name('smartdata.monitoreo');
